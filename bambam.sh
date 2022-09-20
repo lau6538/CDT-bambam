d529f64c6383b593c4fadf3e1f2bbb44cfe3f97e
Logan Upchurch <lau6538@rit.edu>
2022-09-20 19:48:32 -0400

    first option complete

--- bambam.sh
+++ bambam.sh
@@ -3,32 +3,41 @@
 # Bamboozle (bambam)
 # Logan Upchurch
 
+# quit if not run as root
 if [ "$EUID" -ne 0 ]; then
 	echo "This script only works as root"
-	exit
+	exit -1
 fi
 
-clear
+# make test empty folder in /tmp
+DIR="/tmp/bam"
+mkdir -p $DIR
 
-# make empty folder in /tmp
-printf "Making empty directory /tmp/bam\n"
-mkdir -p /tmp/bam
-
-# if it can't, make empty folder in /usr/bin
+# if it can't, make test empty folder in /usr/bin
 if [ $? -ne 0 ]; then
-	printf "Failed. Making empty directory /usr/bin/bam\n"
-	mkdir -p /usr/bin/bam
+	DIR="/usr/bin/bam"
+	mkdir -p $DIR
+	
+	# if it can't, quit
+	if [ $? -ne 0 ]; then
+		echo "Script failed"
+		exit -2
+	fi
+	
+	rmdir $DIR 2> /dev/null
 fi
 
-printf "Success. Press any key to continue\n"
-read -n 1 -s
+# remove test folder
+rmdir $DIR 2> /dev/null
 
 clear
 printf "*********************\n"
 printf "***** BAMBOOZLE *****\n"
-printf "*********************\n\n\n"
+printf "*********************\n\n"
 
-printf "Enter selection:\n\n"
+printf "The empty directory is: $DIR\n\n"
+
+printf "Enter selection:\n"
 
 OPTIONS=("Hide a specific process" "Hide random processes" "Quit")
 
@@ -36,7 +45,21 @@ select opt in "${OPTIONS[@]}"
 do
 	case $opt in
 		"Hide a specific process")
-			printf "1"
+			clear
+			printf "Enter the PID of the process you want to hide\n"
+			printf "(If you're unsure, do ps -ef)\n"
+			read pid
+			
+			mkdir -p ${DIR}${pid}
+			mount -o bind ${DIR}${pid} /proc/$pid
+			
+			if [ $? -ne 0 ]; then
+				printf "Mount failed. Press any key to return to menu\n"
+				read -n 1 -s
+			else
+				printf "Success. Press any key to return to menu\n"
+				read -n 1 -s
+			fi
 			;;
 		"Hide random processes")
 			printf "2"
@@ -48,9 +71,11 @@ do
 			clear
 			printf "*********************\n"
 			printf "***** BAMBOOZLE *****\n"
-			printf "*********************\n\n\n"
+			printf "*********************\n\n"
 			
-			printf "Enter selection:\n\n"
+			printf "The empty directory is: $DIR\n\n"
+			
+			printf "Enter selection:\n"
 			printf "1) Hide a specific process\n"
 			printf "2) Hide random processes\n"
 			printf "3) Quit\n"
