4ddc40dab0ab0b50e6acf0475f6a0f8a54aa07e6
Logan Upchurch <lau6538@rit.edu>
2022-09-20 17:50:57 -0400

    script now adds empty dir

--- bambam.sh
+++ bambam.sh
@@ -3,4 +3,18 @@
 # Bamboozle (bambam)
 # Logan Upchurch
 
+# make empty folder in /tmp
+printf "Making empty directory /tmp/bam\n"
+mkdir -p /tmp/bam
+
+# if it can't, make empty folder in /usr/bin
+if [ $? -ne 0 ]; then
+	printf "Failed. Making empty directory /usr/bin/bam\n"
+	mkdir -p /usr/bin/bam
+fi
+
+printf "Success. Press any key to continue\n"
+read -n 1 -s
+
+
 
