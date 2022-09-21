f45c982b27fef29f254a3057d3fc044f922ce1e7
Logan Upchurch <lau6538@rit.edu>
2022-09-20 20:35:38 -0400

    second option complete

--- bambam.sh
+++ bambam.sh
@@ -30,56 +30,99 @@ fi
 # remove test folder
 rmdir $DIR 2> /dev/null
 
-clear
-printf "*********************\n"
-printf "***** BAMBOOZLE *****\n"
-printf "*********************\n\n"
-
-printf "The empty directory is: $DIR\n\n"
-
-printf "Enter selection:\n"
-
-OPTIONS=("Hide a specific process" "Hide random processes" "Quit")
-
-select opt in "${OPTIONS[@]}"
+while :
 do
+	# clear the terminal and print the menu
+	clear
+	printf "*********************\n"
+	printf "***** BAMBOOZLE *****\n"
+	printf "*********************\n\n"
+	
+	printf "The empty directory is: $DIR\n\n"
+	
+	printf "Enter selection:\n"
+	printf "1) Hide a specific process\n"
+	printf "2) Hide random processes\n"
+	printf "q to quit\n\n"
+	
+	if [ "$opt" = "no" ]; then
+		printf "Invalid option\n"
+	else
+		printf "\n"
+	fi
+
+	# get input
+	printf "#?"
+	read opt
+
 	case $opt in
-		"Hide a specific process")
+		# hide a specific process
+		1)
+			while :
+			do
+				# prompt for process PID
+				clear
+				printf "Enter the PID of the process you want to hide (q to menu)\n"
+				printf "(If you're unsure, do ps -ef)\n"
+				read pid
+				
+				# return to menu
+				if [ "$pid" == "q" ] || [ "$pid" == "Q" ]; then
+					break
+				fi
+				
+				# make an empty directory and mount the process PID dir to that directory
+				mkdir -p ${DIR}${pid}
+				mount -o bind ${DIR}${pid} /proc/$pid
+				
+				# failure, loop back to prompt
+				if [ $? -ne 0 ]; then
+					printf "Mount failed. Press any key to try again\n"
+					read -n 1 -s
+				# success
+				else
+					printf "Success. Press any key to return to menu\n"
+					read -n 1 -s
+					break
+				fi
+			done
+			opt="yes"
+			;;
+		# hide random processes
+		2)
 			clear
-			printf "Enter the PID of the process you want to hide\n"
-			printf "(If you're unsure, do ps -ef)\n"
-			read pid
+			printf "Hiding random processes...\n"
 			
-			mkdir -p ${DIR}${pid}
-			mount -o bind ${DIR}${pid} /proc/$pid
+			ps -ef | while read line
+			do
+				pid=$(awk '{print $2}')
+				if [ "$pid" != "PID" ]; then
+					if [ $RANDOM -gt 16383 ]; then
+						mkdir -p ${DIR}${pid}
+						mount -o bind ${DIR}${pid} /proc/$pid
+						
+						# failure
+						if [ $? -ne 0 ]; then
+							printf "Mount failed for /proc/$pid\n"
+						# success
+						else
+							printf "Process $pid hidden\n"
+						fi
+					fi
+				fi
+			done
 			
-			if [ $? -ne 0 ]; then
-				printf "Mount failed. Press any key to return to menu\n"
-				read -n 1 -s
-			else
-				printf "Success. Press any key to return to menu\n"
-				read -n 1 -s
-			fi
+			printf "Done. Press any key to return to menu\n"
+			read -n 1 -s
+			opt="yes"
 			;;
-		"Hide random processes")
-			printf "2"
-			;;
-		"Quit")
+		# quit
+		q | Q)
 			break
 			;;
+		# invalid option
 		*)
-			clear
-			printf "*********************\n"
-			printf "***** BAMBOOZLE *****\n"
-			printf "*********************\n\n"
-			
-			printf "The empty directory is: $DIR\n\n"
-			
-			printf "Enter selection:\n"
-			printf "1) Hide a specific process\n"
-			printf "2) Hide random processes\n"
-			printf "3) Quit\n"
-			printf "Invalid option\n"
+			opt="no"
 			;;
 	esac
 done
